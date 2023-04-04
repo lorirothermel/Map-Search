ae68575d5179d26231a885bb7ff1606e9f639199
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-04-04 02:37:57 -0400

    Initial Commit

---
+++ Map Search/Map_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_SearchApp.swift
+//  Map Search
+//
+//  Created by Lori Rothermel on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Map_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
